217fbad8b28b88b17ec68614b0af8cf81db415d6
Thomas Jenkins <tjenkins52689@gmail.com>
2015-04-02 12:05:43 -0400

    modify HumanPlayer

--- game.rb
+++ game.rb
@@ -1,18 +1,32 @@
 require_relative 'board.rb'
 require_relative 'piece.rb'
+require_relative 'Human_player.rb'
 
 
 class Checkers
 
-  attr_reader :player1, :player2, :board
+  attr_reader :player1, :player2, :board, :current_player
+
+  def self.test
+    tom = HumanPlayer.new("Tom")
+    tom2 = HumanPlayer.new("Tom2")
+    Checkers.new(tom,tom2).play
+  end
+
 
   def initialize(player1,player2, board = Board.default_game_board)
     @player1 = player1
     @player2 = player2
-
+    @board = board
     @current_player = player1
     set_colors
+    pass_board
   end
+
+  def pass_board
+    player1.get_board(board)
+    player2.get_board(board)
+
 
   def set_colors
     @player1.set_color(:red)
@@ -40,10 +54,11 @@ class Checkers
 
   def get_move
     begin
-      start_pos = current_player.ask_move
+      start_pos = current_player.ask_move(:start)
       piece = board[start_pos]
+      raise "NOT YOUR PIECE" if self.current_player.color != piece.color
       p piece.moves
-      end_pos = current_player.ask_move
+      end_pos = current_player.ask_move(:end_move)
       if (start_pos[0] - end_pos[0]).abs == 2
         perform_jump(start_pos, end_pos)
       else
@@ -58,7 +73,7 @@ class Checkers
 
   def perform_jump(start_pos, end_pos = nil)
     begin
-      end_pos = end_pos || current_player.ask_move
+      end_pos = end_pos || current_player.ask_move(:end_move)
       piece = board[start_pos]
       board.jump(start_pos, end_pos)
       if piece.jumps.any?
--- human_player.rb
+++ human_player.rb
@@ -1,11 +1,11 @@
 
 class HumanPlayer
+  attr_reader :color, :name, :board
 
-
-  PROMPTS {
-    start: "Please enter the position of the piece you would like to move",
-    end_pos: "Please enter the position you would like to move that piece to"
-    name: "Please enter your name"
+  PROMPTS= {
+    start:"Please enter the position of the piece you would like to move",
+    end_pos:"Please enter the position you would like to move that piece to",
+    name:"Please enter your name"
   }
 
   def initialize(name = nil)
@@ -13,12 +13,21 @@ class HumanPlayer
   end
 
   def set_color(color)
-    self.color = color
+    @color = color
   end
 
-  def ask(prompt)
-    puts PROMPTS[prompt]
+  def get_board(board)
+    @board = board
+  end
+
+  def ask_name
+    puts PROMPTS[:name]
     gets.chomp
+  end
+
+  def ask_move(prompt)
+    puts PROMPTS[prompt]
+    gets.chomp.split("").map(&:to_i)
   end
 
 end
